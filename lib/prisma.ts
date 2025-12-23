c833a1661964309adac4dbb410df1afedc7b80e3
prashxant <notxdead0@gmail.com>
2025-12-23 12:07:53 +0530

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from "@prisma/client/extension";
 
-
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query", "error", "warn"],
-  });
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+
+export default prisma; 
